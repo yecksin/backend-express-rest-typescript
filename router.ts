25e37152b138d62b28c7a3f1fe359da2107a8b1a
Yecksin <yecksin@gmail.com>
2020-11-26 20:00:07 -0500

    Creacion de home con rutas

--- router.ts
+++ router.ts
@@ -5,9 +5,75 @@ import * as PokemonsController from './src/controllers/PokemonController';
 export const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World with Typescript!')
-})
+    res.send(`
+    <p
+    style="
+    font-family: fantasy;
+    text-align: center; 
+    font-size: 30px;
+    height: 100px;
+    margin-top: 50px ;
+    "
+    > En el 
+    <a target=”_blank” href="https://github.com/yecksin/backend-express-rest-typescript" >
+      
+      Repositorio github</a> Se encuentra la documentacion de los parametros
+    </p>
 
+    <h1
+    style="
+    font-family: fantasy;
+    text-align: center; 
+    font-size: 30px;
+    height: 100px;
+
+    "
+    >Ejemplo de rutas</h1>
+
+    <div
+    style="
+    font-family: fantasy;
+    text-align: center; 
+    font-size: 20px;
+    height: 100px;
+
+    "
+    >
+   <h1 style="color: orange">Digimons</h1>
+   <h3>Mostrar todos</h3>
+    <a target=”_blank” href="http://localhost:3000/digimons" >http://localhost:3000/digimons</a>
+    <h3>Buscar por id</h3>
+    <a target=”_blank” href="http://localhost:3000/digimons/1" >http://localhost:3000/digimons/1</a>
+    <h3>Buscar por tipo</h3>
+    <a target=”_blank” href="http://localhost:3000/digimons/type/va" >http://localhost:3000/digimons/type/va</a>
+    <h3>Dice cual es mas fuerte usando códigos</h3>
+    <a target=”_blank” href="http://localhost:3000/digimons/strong/1/2" >http://localhost:3000/digimons/strong/1/2</a>
+    <h3>Agregar</h3>
+    <a target=”_blank” href="http://localhost:3000/digimons/save/Agumon/500/Vacuna/Fuego/Virus/veneno.img" >http://localhost:3000/digimons/save/Agumon/500/Vacuna/Fuego/Virus/veneno.img</a>
+   
+    <h1 style="color: red" >Pokemons</h1>
+    <h3>Mostrar todos</h3>
+     <a target=”_blank” href="http://localhost:3000/pokemons" >http://localhost:3000/pokemons</a>
+     <h3>Buscar por id</h3>
+     <a target=”_blank” href="http://localhost:3000/pokemons/1" >http://localhost:3000/pokemons/1</a>
+     <h3>Buscar por tipo</h3>
+     <a target=”_blank” href="http://localhost:3000/pokemons/type/fue" >http://localhost:3000/pokemons/type/fue</a>
+     <h3>Dice cual es mas fuerte usando códigos</h3>
+     <a target=”_blank” href="http://localhost:3000/pokemons/strong/1/2" >http://localhost:3000/pokemons/strong/1/2</a>
+     <h3>Agregar</h3>
+     <a target=”_blank” href="http://localhost:3000/pokemons/save/Raichu/500/Electrico/Planta/Tierra/Raichu.img" >http://localhost:3000/pokemons/save/Raichu/500/Electrico/Planta/Tierra/Raichu.img</a>
+     </div>
+   
+    </div>
+
+
+
+
+    `
+
+    )
+})
+// res.status(400).send(error);
 router.get('/ts', (req, res) => {
     res.send('Typescript es lo máximo!')
 })
