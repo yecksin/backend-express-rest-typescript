d176f6846b5f958761724f0f18d58ca5e01de7b2
Yecksin <yecksin@gmail.com>
2020-11-26 20:11:05 -0500

    cambio de ubicacion ruta principal

--- router.ts
+++ router.ts
@@ -4,6 +4,31 @@ import * as PokemonsController from './src/controllers/PokemonController';
 
 export const router = express.Router();
 
+
+router.get('/ts', (req, res) => {
+    res.send('Typescript es lo máximo!')
+})
+
+router.get('/digimons', DigimonsController.getAll);
+router.get('/digimons/:id', DigimonsController.get);
+router.get('/digimons/name/:name', DigimonsController.getByName);
+router.get('/digimons/type/:type', DigimonsController.getByType);
+router.get('/digimons/strong/:id1/:id2', DigimonsController.getMoreStrong);
+router.get('/digimons/save/:name/:damage/:type/:strongAgainst/:weakAgainst/:img', DigimonsController.saveDigimon);
+
+router.get('/pokemons', PokemonsController.getAll);
+router.get('/pokemons/:id', PokemonsController.get);
+router.get('/pokemons/name/:name', PokemonsController.getByName);
+router.get('/pokemons/type/:type', PokemonsController.getByType);
+router.get('/pokemons/strong/:id1/:id2', PokemonsController.getMoreStrong);
+router.get('/pokemons/save/:name/:damage/:type/:strongAgainst/:weakAgainst/:img', PokemonsController.savePokemon);
+
+router.post("/", (req, res) => {
+    console.log("Cuerpo:", req.body);
+    res.status(200).send(req.body);
+});
+
+
 router.get('/', (req, res) => {
     res.send(`
     <p
@@ -73,26 +98,3 @@ router.get('/', (req, res) => {
 
     )
 })
-// res.status(400).send(error);
-router.get('/ts', (req, res) => {
-    res.send('Typescript es lo máximo!')
-})
-
-router.get('/digimons', DigimonsController.getAll);
-router.get('/digimons/:id', DigimonsController.get);
-router.get('/digimons/name/:name', DigimonsController.getByName);
-router.get('/digimons/type/:type', DigimonsController.getByType);
-router.get('/digimons/strong/:id1/:id2', DigimonsController.getMoreStrong);
-router.get('/digimons/save/:name/:damage/:type/:strongAgainst/:weakAgainst/:img', DigimonsController.saveDigimon);
-
-router.get('/pokemons', PokemonsController.getAll);
-router.get('/pokemons/:id', PokemonsController.get);
-router.get('/pokemons/name/:name', PokemonsController.getByName);
-router.get('/pokemons/type/:type', PokemonsController.getByType);
-router.get('/pokemons/strong/:id1/:id2', PokemonsController.getMoreStrong);
-router.get('/pokemons/save/:name/:damage/:type/:strongAgainst/:weakAgainst/:img', PokemonsController.savePokemon);
-
-router.post("/", (req, res) => {
-    console.log("Cuerpo:", req.body);
-    res.status(200).send(req.body);
-});
